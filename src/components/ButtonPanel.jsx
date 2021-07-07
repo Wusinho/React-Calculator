2f541b5e1c459dacd4dd1d9fa1ed44611b4f68a6
Heber <heberlazo@gmail.com>
2021-07-07 15:23:24 -0500

    Fix linters

--- src/components/ButtonPanel.jsx
+++ src/components/ButtonPanel.jsx
@@ -3,28 +3,28 @@ import React from 'react';
 import Square from './Square';
 
 const Button = () => (
-    <div>
-        <div className="board-row">
-            <Square value={7} />
-            <Square value={8} />
-            <Square value={9} />
-        </div>
-        <div className="board-row">
-            <Square value={4} />
-            <Square value={5} />
-            <Square value={6} />
-        </div>
-        <div className="board-row">
-            <Square value={1} />
-            <Square value={2} />
-            <Square value={3} />
-        </div>
-        <div className="board-row">
-            <Square value="." />
-            <Square value={0} />
-            <Square value="=" />
-        </div>
+  <div>
+    <div className="board-row">
+      <Square value={7} />
+      <Square value={8} />
+      <Square value={9} />
     </div>
+    <div className="board-row">
+      <Square value={4} />
+      <Square value={5} />
+      <Square value={6} />
+    </div>
+    <div className="board-row">
+      <Square value={1} />
+      <Square value={2} />
+      <Square value={3} />
+    </div>
+    <div className="board-row">
+      <Square value="." />
+      <Square value={0} />
+      <Square value="=" />
+    </div>
+  </div>
 );
 
-export default Button
+export default Button;
