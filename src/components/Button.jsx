0cc0a8b3972c5da7d24b7aa09a0b76d0200a3a76
Heber <heberlazo@gmail.com>
2021-07-14 11:01:46 -0500

    Changed button arguments names

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Squarebtn = ({ buttonName, handlerClick }) => (<button type="button" classbuttonName="square" handlerClick={handlerClick}>{buttonName}</button>);
+const Squarebtn = ({ buttonName, handlerClick, type }) => {
+  const handleClick = () => handlerClick(buttonName);
+  let color = '';
+  if (type === 'sp') {
+    color = 'purple';
+  } if (type === 'op') {
+    color = 'blue';
+  }
+
+  return (
+    <button type="button" className={`square ${color}`} classbuttonName="square" handlerClick={handleClick}>{buttonName}</button>
+  );
+};
 
 Squarebtn.propTypes = {
   buttonName: PropTypes.string,
   handlerClick: PropTypes.func.isRequired,
+  type: PropTypes.string,
+
 };
 
 Squarebtn.defaultProps = {
   buttonName: '0',
+  type: null,
 };
 
 export default Squarebtn;
