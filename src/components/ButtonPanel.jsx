01f8c9ab5461c879a884407fecf6710b96ce554e
Heber <heberlazo@gmail.com>
2021-07-14 11:33:28 -0500

    Added some margins to buttonpanel

--- src/components/ButtonPanel.jsx
+++ src/components/ButtonPanel.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
-import '../index.css';
 
 const ButtonPanel = ({ clickHandler }) => {
   const clickHandle = (buttonName) => {
@@ -9,31 +8,31 @@ const ButtonPanel = ({ clickHandler }) => {
   };
   return (
     <>
-      <div className="d-grid">
+      <div className="board-row">
         <Button buttonName="AC" handlerClick={clickHandle} type="sp" />
         <Button buttonName="+/-" handlerClick={clickHandle} type="sp" />
         <Button buttonName="%" handlerClick={clickHandle} type="sp" />
         <Button buttonName="/" handlerClick={clickHandle} type="op" />
       </div>
-      <div className="d-grid">
+      <div className="board-row">
         <Button buttonName="7" handlerClick={clickHandle} type="num" />
         <Button buttonName="8" handlerClick={clickHandle} type="num" />
         <Button buttonName="9" handlerClick={clickHandle} type="num" />
         <Button buttonName="X" handlerClick={clickHandle} type="op" />
       </div>
-      <div className="d-grid">
+      <div className="board-row">
         <Button buttonName="4" handlerClick={clickHandle} type="num" />
         <Button buttonName="5" handlerClick={clickHandle} type="num" />
         <Button buttonName="6" handlerClick={clickHandle} type="num" />
         <Button buttonName="-" handlerClick={clickHandle} type="op" />
       </div>
-      <div className="d-grid">
+      <div className="board-row">
         <Button buttonName="1" handlerClick={clickHandle} type="num" />
         <Button buttonName="2" handlerClick={clickHandle} type="num" />
         <Button buttonName="3" handlerClick={clickHandle} type="num" />
         <Button buttonName="+" handlerClick={clickHandle} type="op" />
       </div>
-      <div className="d-grid-last">
+      <div className="board-row-last">
         <Button buttonName="0" handlerClick={clickHandle} type="num" />
         <Button buttonName="." handlerClick={clickHandle} type="num" />
         <Button buttonName="=" handlerClick={clickHandle} type="op" />
