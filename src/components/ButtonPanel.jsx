12928f035966d6b295781adef50b5630b40a2868
Heber <heberlazo@gmail.com>
2021-07-14 11:25:43 -0500

    Changed ButtonPanel display

--- src/components/ButtonPanel.jsx
+++ src/components/ButtonPanel.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
 import Button from './Button';
 
-const ButtonPanel = (clickhandle) => (
+const ButtonPanel = () => (
   <>
     <div className="board-row">
-      <Button buttonName="AC" onClick={clickhandle} type="sp" />
-      <Button buttonName="+/-" onClick={clickhandle} type="sp" />
-      <Button buttonName="%" onClick={clickhandle} type="sp" />
-      <Button buttonName="/" onClick={clickhandle} type="op" />
+      <Button buttonName="AC" type="sp" />
+      <Button buttonName="+/-" type="sp" />
+      <Button buttonName="%" type="sp" />
+      <Button buttonName="/" type="op" />
     </div>
     <div className="board-row">
-      <Button buttonName="7" onClick={clickhandle} type="num" />
-      <Button buttonName="8" onClick={clickhandle} type="num" />
-      <Button buttonName="9" onClick={clickhandle} type="num" />
-      <Button buttonName="X" onClick={clickhandle} type="op" />
+      <Button buttonName="7" type="num" />
+      <Button buttonName="8" type="num" />
+      <Button buttonName="9" type="num" />
+      <Button buttonName="X" type="op" />
     </div>
     <div className="board-row">
-      <Button buttonName="4" onClick={clickhandle} type="num" />
-      <Button buttonName="5" onClick={clickhandle} type="num" />
-      <Button buttonName="6" onClick={clickhandle} type="num" />
-      <Button buttonName="-" onClick={clickhandle} type="op" />
+      <Button buttonName="4" type="num" />
+      <Button buttonName="5" type="num" />
+      <Button buttonName="6" type="num" />
+      <Button buttonName="-" type="op" />
     </div>
     <div className="board-row">
-      <Button buttonName="1" onClick={clickhandle} type="num" />
-      <Button buttonName="2" onClick={clickhandle} type="num" />
-      <Button buttonName="3" onClick={clickhandle} type="num" />
-      <Button buttonName="+" onClick={clickhandle} type="op" />
+      <Button buttonName="1" type="num" />
+      <Button buttonName="2" type="num" />
+      <Button buttonName="3" type="num" />
+      <Button buttonName="+" type="op" />
     </div>
     <div className="board-row-last">
-      <Button buttonName="0" onClick={clickhandle} type="num" />
-      <Button buttonName="." onClick={clickhandle} type="num" />
-      <Button buttonName="=" onClick={clickhandle} type="op" />
-      <Button buttonName="" onClick={clickhandle} type="" />
+      <Button buttonName="0" type="num" />
+      <Button buttonName="." type="num" />
+      <Button buttonName="=" type="op" />
+      <Button buttonName="" type="" />
 
     </div>
   </>
