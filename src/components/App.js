20c3b47bb4e391b8703618b94ce6d6c241b61811
Heber <heberlazo@gmail.com>
2021-07-14 11:37:12 -0500

    Refactor methods in calculate js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import ButtonPanel from './ButtonPanel';
 import Display from './Display';
-// import Calculate from '../logic/calculate';
-import '../assets/styles/App.scss';
+import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
 
-const App = () => (
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
 
-  <>
-    <Display />
-    <ButtonPanel />
-  </>
-);
+  render() {
+    const handleClick = (buttonName) => {
+      const result = calculate(this.state, buttonName);
+      this.setState({ ...result });
+    };
+    const { next, total } = this.state;
 
+    return (
+      <div>
+        <Display result={(next && next.toString()) || (total && total.toString())} />
+        <ButtonPanel clickHandler={(e) => handleClick(e)} />
+      </div>
+    );
+  }
+}
 export default App;
