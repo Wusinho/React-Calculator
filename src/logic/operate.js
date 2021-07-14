0d83645144cf1251c0e40685a266e102ab285fbe
Heber <heberlazo@gmail.com>
2021-07-14 10:46:03 -0500

    Added operate.js

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,27 +1,27 @@
 import Big from 'big.js';
 
-const mathFunctions = (a, b, symbol) => {
-  const n1 = new Big(a || 0);
-  const n2 = new Big(b || 0);
+const calculate = (numberOne, numberTwo, operation) => {
+  const n1 = new Big(numberOne || 0);
+  const n2 = new Big(numberTwo || 0);
   const n3 = new Big(100);
   let result = 0;
-  if (symbol === '/') {
+  if (operation === '/') {
     result = (n1.div(n2));
     return result.valueOf();
-  } if (symbol === 'X') {
+  } if (operation === 'X') {
     result = (n1.times(n2));
     return result.valueOf();
-  } if (symbol === '-') {
+  } if (operation === '-') {
     result = (n1.minus(n2));
     return result.valueOf();
-  } if (symbol === '+') {
+  } if (operation === '+') {
     result = (n1.plus(n2));
     return result.valueOf();
-  } if (symbol === '%') {
+  } if (operation === '%') {
     result = (n1.div(n3));
     return result.valueOf();
   }
   return result;
 };
 
-export { mathFunctions as default };
+export { calculate as default };
