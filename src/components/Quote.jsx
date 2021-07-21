2ee92f4f50cbf74b4961d1ef8ef0c0036637f92f
Heber <heberlazo@gmail.com>
2021-07-21 15:35:22 -0500

    Added quote

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const Quote = () => (
   <div className="quote">
     <h1> QUOTE</h1>
-    <p>It is a good day to die</p>
+    <p>
+      “Any fool can write code that a computer can understand.
+      Good programmers write code that humans can understand.”
+    </p>
+    <p>– Martin Fowler</p>
   </div>
 );
 
