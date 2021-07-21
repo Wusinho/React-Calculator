b213f2b25ab4de0c6cc00a70b609af1d1c170747
Heber <heberlazo@gmail.com>
2021-07-21 12:07:01 -0500

    Corrected Routes and navbar

---
+++ src/components/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+  BrowserRouter as Router, Switch, Route, Link,
+} from 'react-router-dom';
+import Calculator from './Calculator';
+import Home from './Home';
+import Quote from './Quote';
+
+import '../assets/styles/App.scss';
+
+const App = () => (
+  <Router>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/calculator">Calculator</Link>
+        </li>
+        <li>
+          <Link to="/quote">Quote</Link>
+        </li>
+      </ul>
+    </nav>
+    <Switch>
+      <Route path="/calculator">
+        <Calculator />
+      </Route>
+      <Route path="/quote">
+        <Quote />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App  from "./components/App"
 
-ReactDOM.render(<App />, document.getElementById("app"));
+// ReactDOM.render(<App />, document.getElementById("app"));
+
+ReactDOM.render(
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>,
+    document.getElementById('app')
+  );
